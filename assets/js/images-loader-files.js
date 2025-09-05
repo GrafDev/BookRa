952583e976d665f93e045c6d46d8744154539a5c
grafDev <graf@dev.com>
2025-09-05 22:08:03 +0200

    fix load images

--- assets/js/images-loader-files.js
+++ assets/js/images-loader-files.js
@@ -1,71 +1,113 @@
-// Regular file path imports for images (no base64)
+// Import images properly so Vite can process them
+import bgDesktop from '../images/bg-desktop.webp';
+import bgMobile from '../images/bg-mobile.webp';
+import bgMain from '../images/bg-main.webp';
+import man1Part1 from '../images/man1-part1.webp';
+import man2 from '../images/man2.webp';
+import man3Part1 from '../images/man3-part1.webp';
+import man3Part2 from '../images/man3-part2.webp';
+import man4Part1 from '../images/man4-part1.webp';
+import man4Part2 from '../images/man4-part2.webp';
+import logo1Part1 from '../images/logo1-part1.png';
+import logo1Part2 from '../images/logo1-part2.png';
+import logo2Part1 from '../images/logo2-part1.webp';
+import logo2Part2 from '../images/logo2-part2.png';
+import title from '../images/title.png';
+import titleMobile from '../images/title-mobile.webp';
+import arrowPart1 from '../images/arrow-part1.png';
+import arrowPart2 from '../images/arrow-part2.png';
+import wheelPart1 from '../images/wheel-part1.webp';
+import wheelPart2 from '../images/wheel-part2.png';
+import wheelPart3 from '../images/wheel-part3.webp';
+import wheelPart31 from '../images/wheel-part3-1.webp';
+import wheelPart4 from '../images/wheel-part4.webp';
+import wheelPart5 from '../images/wheel-part5.webp';
+import wheelPart6 from '../images/wheel-part6.webp';
+import wheelText1 from '../images/wheel-text1.png';
+import wheelText2 from '../images/wheel-text2.png';
+import firstCart from '../images/first-cart.png';
+import secondCart from '../images/second-cart.png';
+import thirdCart from '../images/third-cart.png';
+import backCart from '../images/back-cart.webp';
+import blanketCart from '../images/blanket-cart.webp';
+import borderCart from '../images/border-cart.webp';
+import scratch from '../images/scratch.png';
+import playMusic from '../images/play-music.png';
+import stopMusic from '../images/stop-music.png';
+import wheelModalText from '../images/wheel-modal-text.png';
+import scratchModalText from '../images/scratch-modal-text.png';
+import buttonModal from '../images/button-modal.png';
+import bgModalLeft from '../images/bg-modal-left.webp';
+import bgModalCenter from '../images/bg-modal-center.webp';
+import bgModalRight from '../images/bg-modal-right.webp';
+
 const images = {
-  bgDesktop: './assets/images/bg-desktop.webp',
-  bgMobile: './assets/images/bg-mobile.webp',
-  bgMain: './assets/images/bg-main.webp',
-  man1Part1: './assets/images/man1-part1.webp',
-  man2: './assets/images/man2.webp',
-  man3Part1: './assets/images/man3-part1.webp',
-  man3Part2: './assets/images/man3-part2.webp',
-  man4Part1: './assets/images/man4-part1.webp',
-  man4Part2: './assets/images/man4-part2.webp',
-  logo1Part1: './assets/images/logo1-part1.png',
-  logo1Part2: './assets/images/logo1-part2.png',
-  logo2Part1: './assets/images/logo2-part1.webp',
-  logo2Part2: './assets/images/logo2-part2.png',
-  title: './assets/images/title.png',
-  titleMobile: './assets/images/title-mobile.webp',
-  arrowPart1: './assets/images/arrow-part1.png',
-  arrowPart2: './assets/images/arrow-part2.png',
-  wheelPart1: './assets/images/wheel-part1.webp',
-  wheelPart2: './assets/images/wheel-part2.png',
-  wheelPart3: './assets/images/wheel-part3.webp',
-  wheelPart31: './assets/images/wheel-part3-1.webp',
-  wheelPart4: './assets/images/wheel-part4.webp',
-  wheelPart5: './assets/images/wheel-part5.webp',
-  wheelPart6: './assets/images/wheel-part6.webp',
-  wheelText1: './assets/images/wheel-text1.png',
-  wheelText2: './assets/images/wheel-text2.png',
-  firstCart: './assets/images/first-cart.png',
-  secondCart: './assets/images/second-cart.png',
-  thirdCart: './assets/images/third-cart.png',
-  backCart: './assets/images/back-cart.webp',
-  blanketCart: './assets/images/blanket-cart.webp',
-  borderCart: './assets/images/border-cart.webp',
-  scratch: './assets/images/scratch.png',
-  playMusic: './assets/images/play-music.png',
-  stopMusic: './assets/images/stop-music.png',
+  bgDesktop,
+  bgMobile,
+  bgMain,
+  man1Part1,
+  man2,
+  man3Part1,
+  man3Part2,
+  man4Part1,
+  man4Part2,
+  logo1Part1,
+  logo1Part2,
+  logo2Part1,
+  logo2Part2,
+  title,
+  titleMobile,
+  arrowPart1,
+  arrowPart2,
+  wheelPart1,
+  wheelPart2,
+  wheelPart3,
+  wheelPart31,
+  wheelPart4,
+  wheelPart5,
+  wheelPart6,
+  wheelText1,
+  wheelText2,
+  firstCart,
+  secondCart,
+  thirdCart,
+  backCart,
+  blanketCart,
+  borderCart,
+  scratch,
+  playMusic,
+  stopMusic,
   // Modal images also in main images object for easier access
-  wheelModalText: './assets/images/wheel-modal-text.png',
-  scratchModalText: './assets/images/scratch-modal-text.png',
-  buttonModal: './assets/images/button-modal.png'
+  wheelModalText,
+  scratchModalText,
+  buttonModal
 }
 
 const modalImages = {
-  bgModalLeft: './assets/images/bg-modal-left.webp',
-  bgModalCenter: './assets/images/bg-modal-center.webp',
-  bgModalRight: './assets/images/bg-modal-right.webp',
-  buttonModal: './assets/images/button-modal.png',
-  scratchModalText: './assets/images/scratch-modal-text.png',
-  wheelModalText: './assets/images/wheel-modal-text.png'
+  bgModalLeft,
+  bgModalCenter,
+  bgModalRight,
+  buttonModal,
+  scratchModalText,
+  wheelModalText
 }
 
 const criticalImages = {
-  bgDesktop: './assets/images/bg-desktop.webp',
-  bgMobile: './assets/images/bg-mobile.webp',
-  bgMain: './assets/images/bg-main.webp',
-  man1Part1: './assets/images/man1-part1.webp',
-  man2: './assets/images/man2.webp',
-  man3Part1: './assets/images/man3-part1.webp',
-  man3Part2: './assets/images/man3-part2.webp',
-  man4Part1: './assets/images/man4-part1.webp',
-  man4Part2: './assets/images/man4-part2.webp',
-  logo1Part1: './assets/images/logo1-part1.png',
-  logo1Part2: './assets/images/logo1-part2.png',
-  logo2Part1: './assets/images/logo2-part1.webp',
-  logo2Part2: './assets/images/logo2-part2.png',
-  title: './assets/images/title.png',
-  titleMobile: './assets/images/title-mobile.webp'
+  bgDesktop,
+  bgMobile,
+  bgMain,
+  man1Part1,
+  man2,
+  man3Part1,
+  man3Part2,
+  man4Part1,
+  man4Part2,
+  logo1Part1,
+  logo1Part2,
+  logo2Part1,
+  logo2Part2,
+  title,
+  titleMobile
 }
 
 export {
